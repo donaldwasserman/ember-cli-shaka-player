9e366b79af212f2348db4ca0fdf493bc453b9891
Dulmandakh <dulmandakh@gmail.com>
2017-07-06 22:32:33 +0800

    install and import shaka-player into vendor.js

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var fbTransform = require('fastboot-transform');
+
 module.exports = {
-  name: 'ember-cli-shaka-player'
+  name: 'ember-cli-shaka-player',
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import({
+      development: path.join('vendor', 'shaka-player.compiled.debug.js'),
+      production: path.join('vendor', 'shaka-player.compiled.js'),
+    });
+  },
+  treeForVendor(vendorTree) {
+    this._super.treeForVendor.apply(this, arguments);
+    var trees = [];
+    var modulePath = path.join(path.dirname(require.resolve('shaka-player')));
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(
+      fbTransform(new Funnel(modulePath))
+    );
+    return mergeTrees(trees);
+  }
 };
